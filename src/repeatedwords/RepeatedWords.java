3c58148069c975ed80355f467a0316e854434bdb
David Santiago Barrera <d4v1x.sb@gmail.com>
2013-05-18 16:18:15 +0100

    Refactor: New method identify "extractWords".

--- src/repeatedwords/RepeatedWords.java
+++ src/repeatedwords/RepeatedWords.java
@@ -6,19 +6,23 @@ import java.util.List;
 public class RepeatedWords {
 
     public static String[] find(String T1, String T2) {
-        String[] tokensT1 = T1.split("[ ,.;:]");
-        String[] tokensT2 = T2.split("[ ,.;:]");
+        String[] tokensT1 = extractWords(T1);
+        String[] tokensT2 = extractWords(T2);
         List<String> repetedWordsList = new ArrayList<>();
         
-        for(String wordT1 : tokensT1){
-            for (String wordT2 : tokensT2){
-                if(wordT1.equals(wordT2)){
-                    if(!repetedWordsList.contains(wordT1) && !wordT1.isEmpty()){
-                         repetedWordsList.add(wordT1);
+        for(String wordT1 : tokensT1){//escoger
+            for (String wordT2 : tokensT2){//buscar
+                if(wordT1.equals(wordT2)){//si es igual
+                    if(!repetedWordsList.contains(wordT1) && !wordT1.isEmpty()){//si contiene
+                         repetedWordsList.add(wordT1);//añadir
                     }
                 }
             }
         }
         return repetedWordsList.toArray(new String[repetedWordsList.size()]); 
+    }
+
+    private static String[] extractWords(String T1) {
+        return T1.split("[ ,.;:]");
     }
 }
