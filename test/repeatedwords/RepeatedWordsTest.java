95fb049d90f520b7ba7c3e6e76cfc70680fce9cc
David Santiago Barrera <d4v1x.sb@gmail.com>
2013-05-22 00:20:18 +0100

    Change Test to change object ReapeatedWords.

--- test/repeatedwords/RepeatedWordsTest.java
+++ test/repeatedwords/RepeatedWordsTest.java
@@ -1,37 +1,30 @@
 package repeatedwords;
 
-import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import org.junit.Test;
 import static org.junit.Assert.*;
+import org.junit.Test;
 
 public class RepeatedWordsTest {
 
-    private String T1;
-    private String T2;
     private RepeatedWords repeatedWords;
 
-    public RepeatedWordsTest() {
-        repeatedWords = new RepeatedWords();
-    }
-
     public void WordsExample1() {
-        T1 = "en un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo";
-        T2 = "ponte en mi lugar, no tengo mucho tiempo para acordarme de tu nombre";
+        String T1 = "en un lugar de la Mancha, de cuyo nombre no quiero acordarme, no ha mucho tiempo que vivía un hidalgo";
+        String T2 = "ponte en mi lugar, no tengo mucho tiempo para acordarme de tu nombre";
+        repeatedWords = new RepeatedWords(T1, T2);
     }
 
     @Test
     public void numberOfWordsRepeated() {
         WordsExample1();
-
-        assertEquals(8, repeatedWords.find(T1, T2).length);
+        assertEquals(8, repeatedWords.find().length);
     }
 
     @Test
     public void containedOfWordsRepeated() {
         WordsExample1();
-        List<String> listwords = Arrays.asList(repeatedWords.find(T1, T2));
+        List<String> listwords = Arrays.asList(repeatedWords.find());
         String[] solucion = {"acordarme", "de", "en", "lugar", "mucho", "no", "nombre", "tiempo"};
         for (String word : solucion) {
             assertTrue(listwords.contains(word));
